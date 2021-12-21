19b97fed295c3a85ec70fb70d9c88b02d8eee928
danielSbastos <daniel.bastos1710@gmail.com>
2021-12-21 14:20:12 -0300

    Increase sample data

--- ranking-web/src/sampleData.js
+++ ranking-web/src/sampleData.js
@@ -7,45 +7,130 @@ const sampleData = {
     },
     2: {
       'Aspecto 1': 'Básico',
-      'Aspecto 2': 'Médio',
-      'Aspecto 3': 'Básico',
+      'Aspecto 2': 'Básico',
+      'Aspecto 3': 'Médio',
     },
     3: {
       'Aspecto 1': 'Básico',
       'Aspecto 2': 'Básico',
-      'Aspecto 3': 'Médio',
+      'Aspecto 3': 'Avançado',
     },
     4: {
       'Aspecto 1': 'Básico',
       'Aspecto 2': 'Médio',
-      'Aspecto 3': 'Médio',
+      'Aspecto 3': 'Básico',
     },
     5: {
       'Aspecto 1': 'Básico',
       'Aspecto 2': 'Médio',
-      'Aspecto 3': 'Avançado',
+      'Aspecto 3': 'Médio',
     },
     6: {
-      'Aspecto 1': 'Médio',
+      'Aspecto 1': 'Básico',
       'Aspecto 2': 'Médio',
       'Aspecto 3': 'Avançado',
     },
     7: {
+      'Aspecto 1': 'Básico',
+      'Aspecto 2': 'Avançado',
+      'Aspecto 3': 'Básico',
+    },
+    8: {
+      'Aspecto 1': 'Básico',
+      'Aspecto 2': 'Avançado',
+      'Aspecto 3': 'Médio',
+    },
+    9: {
+      'Aspecto 1': 'Básico',
+      'Aspecto 2': 'Avançado',
+      'Aspecto 3': 'Avançado',
+    },
+    10: {
+      'Aspecto 1': 'Médio',
+      'Aspecto 2': 'Básico',
+      'Aspecto 3': 'Básico',
+    },
+    11: {
+      'Aspecto 1': 'Médio',
+      'Aspecto 2': 'Básico',
+      'Aspecto 3': 'Médio',
+    },
+    12: {
+      'Aspecto 1': 'Médio',
+      'Aspecto 2': 'Básico',
+      'Aspecto 3': 'Avançado',
+    },
+    13: {
+      'Aspecto 1': 'Médio',
+      'Aspecto 2': 'Médio',
+      'Aspecto 3': 'Básico',
+    },
+    14: {
+      'Aspecto 1': 'Médio',
+      'Aspecto 2': 'Médio',
+      'Aspecto 3': 'Médio',
+    },
+    15: {
+      'Aspecto 1': 'Médio',
+      'Aspecto 2': 'Médio',
+      'Aspecto 3': 'Avançado',
+    },
+    16: {
+      'Aspecto 1': 'Médio',
+      'Aspecto 2': 'Avançado',
+      'Aspecto 3': 'Básico',
+    },
+    17: {
+      'Aspecto 1': 'Médio',
+      'Aspecto 2': 'Avançado',
+      'Aspecto 3': 'Médio',
+    },
+    18: {
       'Aspecto 1': 'Médio',
       'Aspecto 2': 'Avançado',
       'Aspecto 3': 'Avançado',
     },
-    8: {
+    19: {
+      'Aspecto 1': 'Avançado',
+      'Aspecto 2': 'Básico',
+      'Aspecto 3': 'Básico',
+    },
+    20: {
+      'Aspecto 1': 'Avançado',
+      'Aspecto 2': 'Básico',
+      'Aspecto 3': 'Médio',
+    },
+    21: {
+      'Aspecto 1': 'Avançado',
+      'Aspecto 2': 'Básico',
+      'Aspecto 3': 'Avançado',
+    },
+    22: {
+      'Aspecto 1': 'Avançado',
+      'Aspecto 2': 'Médio',
+      'Aspecto 3': 'Básico',
+    },
+    23: {
+      'Aspecto 1': 'Avançado',
+      'Aspecto 2': 'Médio',
+      'Aspecto 3': 'Médio',
+    },
+    24: {
       'Aspecto 1': 'Avançado',
       'Aspecto 2': 'Médio',
       'Aspecto 3': 'Avançado',
     },
-    9: {
+    25: {
+      'Aspecto 1': 'Avançado',
+      'Aspecto 2': 'Avançado',
+      'Aspecto 3': 'Básico',
+    },
+    26: {
       'Aspecto 1': 'Avançado',
       'Aspecto 2': 'Avançado',
       'Aspecto 3': 'Médio',
     },
-    10: {
+    27: {
       'Aspecto 1': 'Avançado',
       'Aspecto 2': 'Avançado',
       'Aspecto 3': 'Avançado',
