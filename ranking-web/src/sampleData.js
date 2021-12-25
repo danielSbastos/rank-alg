c571f853d284a6882b1267512bbeda6a0252ec4f
danielSbastos <daniel.bastos1710@gmail.com>
2021-12-25 14:32:53 -0300

    add percentage

--- ranking-web/src/sampleData.js
+++ ranking-web/src/sampleData.js
@@ -1,59 +1,144 @@
 const sampleData = {
   objects: {
     1: {
-      'Aspecto 1': 'Básico',
-      'Aspecto 2': 'Básico',
-      'Aspecto 3': 'Básico',
+      'Aspecto 1': 'Nada',
+      'Aspecto 2': 'Nada',
+      'Aspecto 3': 'Nada',
     },
     2: {
-      'Aspecto 1': 'Básico',
-      'Aspecto 2': 'Médio',
+      'Aspecto 1': 'Nada',
+      'Aspecto 2': 'Nada',
       'Aspecto 3': 'Básico',
     },
     3: {
+      'Aspecto 1': 'Nada',
+      'Aspecto 2': 'Nada',
+      'Aspecto 3': 'Médio',
+    },
+    4: {
+      'Aspecto 1': 'Nada',
+      'Aspecto 2': 'Básico',
+      'Aspecto 3': 'Nada',
+    },
+    5: {
+      'Aspecto 1': 'Nada',
+      'Aspecto 2': 'Básico',
+      'Aspecto 3': 'Básico',
+    },
+    6: {
+      'Aspecto 1': 'Nada',
+      'Aspecto 2': 'Básico',
+      'Aspecto 3': 'Médio',
+    },
+    7: {
+      'Aspecto 1': 'Nada',
+      'Aspecto 2': 'Médio',
+      'Aspecto 3': 'Nada',
+    },
+    8: {
+      'Aspecto 1': 'Nada',
+      'Aspecto 2': 'Médio',
+      'Aspecto 3': 'Básico',
+    },
+    9: {
+      'Aspecto 1': 'Nada',
+      'Aspecto 2': 'Médio',
+      'Aspecto 3': 'Médio',
+    },
+    10: {
+      'Aspecto 1': 'Básico',
+      'Aspecto 2': 'Nada',
+      'Aspecto 3': 'Nada',
+    },
+    11: {
+      'Aspecto 1': 'Básico',
+      'Aspecto 2': 'Nada',
+      'Aspecto 3': 'Básico',
+    },
+    12: {
+      'Aspecto 1': 'Básico',
+      'Aspecto 2': 'Nada',
+      'Aspecto 3': 'Médio',
+    },
+    13: {
+      'Aspecto 1': 'Básico',
+      'Aspecto 2': 'Básico',
+      'Aspecto 3': 'Nada',
+    },
+    14: {
+      'Aspecto 1': 'Básico',
+      'Aspecto 2': 'Básico',
+      'Aspecto 3': 'Básico',
+    },
+    15: {
       'Aspecto 1': 'Básico',
       'Aspecto 2': 'Básico',
       'Aspecto 3': 'Médio',
     },
-    4: {
+    16: {
+      'Aspecto 1': 'Básico',
+      'Aspecto 2': 'Médio',
+      'Aspecto 3': 'Nada',
+    },
+    17: {
+      'Aspecto 1': 'Básico',
+      'Aspecto 2': 'Médio',
+      'Aspecto 3': 'Básico',
+    },
+    18: {
       'Aspecto 1': 'Básico',
       'Aspecto 2': 'Médio',
       'Aspecto 3': 'Médio',
     },
-    5: {
-      'Aspecto 1': 'Básico',
-      'Aspecto 2': 'Médio',
-      'Aspecto 3': 'Avançado',
-    },
-    6: {
+    19: {
       'Aspecto 1': 'Médio',
-      'Aspecto 2': 'Médio',
-      'Aspecto 3': 'Avançado',
+      'Aspecto 2': 'Nada',
+      'Aspecto 3': 'Nada',
     },
-    7: {
+    20: {
       'Aspecto 1': 'Médio',
-      'Aspecto 2': 'Avançado',
-      'Aspecto 3': 'Avançado',
+      'Aspecto 2': 'Nada',
+      'Aspecto 3': 'Básico',
     },
-    8: {
-      'Aspecto 1': 'Avançado',
-      'Aspecto 2': 'Médio',
-      'Aspecto 3': 'Avançado',
-    },
-    9: {
-      'Aspecto 1': 'Avançado',
-      'Aspecto 2': 'Avançado',
+    21: {
+      'Aspecto 1': 'Médio',
+      'Aspecto 2': 'Nada',
       'Aspecto 3': 'Médio',
     },
-    10: {
-      'Aspecto 1': 'Avançado',
-      'Aspecto 2': 'Avançado',
-      'Aspecto 3': 'Avançado',
+    22: {
+      'Aspecto 1': 'Médio',
+      'Aspecto 2': 'Básico',
+      'Aspecto 3': 'Nada',
+    },
+    23: {
+      'Aspecto 1': 'Médio',
+      'Aspecto 2': 'Básico',
+      'Aspecto 3': 'Básico',
+    },
+    24: {
+      'Aspecto 1': 'Médio',
+      'Aspecto 2': 'Básico',
+      'Aspecto 3': 'Médio',
+    },
+    25: {
+      'Aspecto 1': 'Médio',
+      'Aspecto 2': 'Médio',
+      'Aspecto 3': 'Nada',
+    },
+    26: {
+      'Aspecto 1': 'Médio',
+      'Aspecto 2': 'Médio',
+      'Aspecto 3': 'Básico',
+    },
+    27: {
+      'Aspecto 1': 'Médio',
+      'Aspecto 2': 'Médio',
+      'Aspecto 3': 'Médio',
     },
     'ideal-candidate': {
-      'Aspecto 1': 'Básico',
-      'Aspecto 2': 'Médio',
-      'Aspecto 3': 'Avançado',
+      'Aspecto 1': 'Nada',
+      'Aspecto 2': 'Básico',
+      'Aspecto 3': 'Médio',
     }
   },
   merits: {
@@ -63,17 +148,17 @@ const sampleData = {
   },
   escalas: {
     'Aspecto 1': {
-      data: ['Básico', 'Médio', 'Avançado'],
+      data: ['Nada', 'Básico', 'Médio'],
       id: 3,
       n: 3
     },
     'Aspecto 2': {
-      data: ['Básico', 'Médio', 'Avançado'],
+      data: ['Nada', 'Básico', 'Médio'],
       id: 3,
       n: 3
     },
     'Aspecto 3': {
-      data: ['Básico', 'Médio', 'Avançado'],
+      data: ['Nada', 'Básico', 'Médio'],
       id: 3,
       n: 3
     },
