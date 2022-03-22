ac3cf5f69bc0841fcf092e83a50a7202329a6dde
danielSbastos <daniel.bastos1710@gmail.com>
2022-03-22 14:20:00 -0300

    fix aws function

--- ranking-web/aws-functions/rank-ic-file/index.js
+++ ranking-web/aws-functions/rank-ic-file/index.js
@@ -1,102 +1,62 @@
-// const percentage = (i, j, delta) => {
-//   let perc = 0
+const rank = (i, j, c) => {
+  let n, d;
 
-//   if (i < j) {
-//     perc = (i - j) / j
-//   } else {
-//     if ((delta - j) > 0) {
-//       perc = (i - j) / (delta - j);
-//     } 
-//   }
-//   return `${(Math.round(perc * 100) * 100) / 100}% `
-// }
+  if (i < j) {
+    n = (i - j)*(j + 1);
+  } else {
+    n = (i - j)*(j + 1)*c;
+  }
 
-// const rank = (i, j, c) => {
-//   let n, d = 0;
+  d = (j + 1);
 
-//   if (i < j) {
-//     n = (i - j)*(j + 1);
-//   } else {
-//     n = (i - j)*(j + 1)*c;
-//   }
 
-//   d = (j + 1);
+  return [n, d]
+}
 
-//   return [n, d]
-// }
+const rankData = (data) => {
+    let idealCandidate = data[0]
+    let candidates = {}
+    data.slice(1, data.length).forEach((candidate, index) => {
+        let n, d, result;
+        n = d = result = 0;
 
-// const rankIndividual = (idealCandidate, candidate, scales) => {
-//   let n, d, _n, _d, delta, merit, percs;
-//   percs = '';
-//   d = n = delta = 0;
+        for (let attr = 1; attr <= Number.parseInt(candidate['n']); attr++) {
+           let i = Number.parseInt(candidate['value'+attr]) / (Number.parseInt(candidate['max'+attr]) - Number.parseInt(candidate['min'+attr]));
+           let j = Number.parseInt(idealCandidate['value'+attr]) / (Number.parseInt(idealCandidate['max'+attr]) - Number.parseInt(idealCandidate['min'+attr]));
+           let merit = candidate['merit'+attr] === 'True' ? 1 : 0
+           result = rank(i, j, merit)
 
-//   candidate.data.forEach((data, idx) => {
-//     delta = scales[candidate.scales[idx]].max - scales[candidate.scales[idx]].min
-//     percs += percentage(data, idealCandidate.data[idx], delta)
+           n += result[0]
+           d += result[1]
 
-//     merit = candidate.merits[idx] ? 1 : 0;
-//     [_n, _d] = rank(data / delta, idealCandidate.data[idx] / delta, merit)
-//     n += _n
-//     d += _d
-//   })
+           console.log(n, d)
+        }
+        candidates[index+1] = n/d
+    })
 
-//   return { rank: n/d, perc: percs };
-// }
+    return candidates
+}
 
-// const rankData = (objects, scales) => {
-//   const ranks = []
-//   const idealCandidate = objects[0]
 
-//   objects.slice(1, objects.length).forEach((object, idx) => {
-//     const { rank, perc } = rankIndividual(idealCandidate, object, scales);
+const multipart = require("parse-multipart");
+const csv = require("csvtojson");
 
-//     if (!isNaN(rank)) {
-//       ranks.push({
-//         rank: Math.round(rank * 10000)/10000,
-//         percentage: perc.trim()
-//       })
-//     }
-//   })
+exports.handler = async event => {
+    const body = Buffer.from(event["content"].toString(), "base64");
+    const headers = event["headers"]["content-type"];
+    const boundary = multipart.getBoundary(headers);
+    const buff = multipart.Parse(body, boundary);
 
-//   return ranks;
-// };
+    const csvDataString = buff[0].data.toString("utf8");
+    const csvData = await csv().fromString(csvDataString);
 
-// const multipart = require("parse-multipart");
-// const csv = require("csvtojson");
+    const ranks = rankData(csvData)
 
-exports.handler = async event => {   
-    console.log(`Event: ${JSON.stringify(event)}`); 
-    const body = Buffer.from(event["content"].toString(), "base64").toString('ascii'); // AWS case    
+    console.log(ranks)
 
-    // const boundary = multipart.getBoundary(event.headers["Content-Type"]);
-    // const buff = multipart.Parse(body, boundary);
-    // const csvDataString = buff[0].data.toString("utf8");
-    // const csvData = await csv().fromString(csvDataString);
-    console.log(body);
-    
     return {
         'statusCode': 200,
-        'body': 'hello' //JSON.stringify(csvData)
+        'body': ranks
     }
 }
 
-// exports.handler = async (event) => {
-//   console.log(event);
-  
-//   const { objects, scales } = event;
-//   const r = rankData(objects, scales)
-   
-//     // TODO implement
-//   const response = {
-//     headers: {
-//       "Access-Control-Allow-Headers" : "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
-//       "Access-Control-Allow-Methods" : "OPTIONS,POST",
-//       "Access-Control-Allow-Credentials" : true,
-//       "Access-Control-Allow-Origin" : "*",
-//       "X-Requested-With" : "*"
-//     },
-//     statusCode: 200,
-//     body: r
-//   };
-//   return response;
-// };
